19844cbe6848a85553e13bb75102260e0f227423
David de Kleer <ddekleer@kpnplanet.nl>
2013-12-05 12:22:33 +0100

    Added a few clarifying comments and changed the file organization a bit.

--- index.php
+++ index.php
@@ -12,18 +12,16 @@
 <h2>Some multiple choice questions</h2>
 
 <?php
-
-/* Debugging: uncomment when needed */
-
+/*
 error_reporting(-1);
-ini_set("display_errors", 1);
-
+ini_set("display_errors", 1); /* Debugging: uncomment if needed */
 
 /* All questions (except the first) come from http://www.pubquizarea.com/
  * view_question_and_answer_quizzes.php?cat_title=general-knowledge&
  * type_title=multiple-choice&cat=32&type=1&&id=6272 
  */
-    
+
+//// DEFINE QUESTIONS AND ANSWERS
 $QandA = array(array("What is the name of the first cheese mentioned in".
          " the <i>Cheese Shop</i> sketch of Monty Python?", "Gouda",
          "Red Leicester", "Mozzarella", "Edam", "Cheshire"),
@@ -39,58 +37,61 @@ $QandA = array(array("What is the name of the first cheese mentioned in".
 $answers = array("B", "C", "E", "A", "D");
 $QandAlen = count($QandA);
 
+//// KEEP TRACK OF THE QUESTION NUMBER
 if(isset($_POST["count"]))
     // get the value of the hidden "count" input field (if it has been set)
     $count = $_POST["count"];
 else
-    $count = 0;
+    // IMPORTANT: $count starts at 1 (because it's question 1), not 0!
+    $count = 1;
 
-if(isset($_POST["next"]) && $count < $QandAlen - 1) 
+if(isset($_POST["next"]) && $count < $QandAlen) 
     // user pressed next button and there are more questions
     $count += 1;
-else if(isset($_POST["prev"]) && $count > 0) 
+else if(isset($_POST["prev"]) && $count > 1) 
     // user pressed previous button and there is a previous question
     $count -= 1;
+ 
+//// SHOW QUESTION CORRESPONDING TO COUNT
+echo "<p>" . $QandA[$count-1][0] . "</p>"; 
 ?>
-
-<p><?php echo $QandA[$count][0]; ?></p>
 
 <form action=index.php method=post>
 
 <?php
+//// SHOW ANSWERS CORRESPONDING TO COUNT
 $answer_values = array('"A"', '"B"', '"C"', '"D"', '"E"');
 for($i = 0; $i < $QandAlen; ++$i) // echo answers to the current question
-    echo '<input type="radio" name="question" value=' . $answer_values[$i] . '/>' . $QandA[$count][$i+1] . '<br/>';
+    echo '<input type="radio" name="question" value=' . $answer_values[$i] . '/>' . $QandA[$count-1][$i+1] . '<br/>';
 echo '<br/>';
-?>
 
-<input type="hidden" name="count" value=<?php echo $count; ?> />
-<input type="submit" name="sub" value="Submit"/>
-
-<?php
-if($count == 0)                  // first question, hide previous button
+//// SHOW/HIDE THE NEXT/PREVIOUS BUTTONS
+if($count == 1)              // first question, hide previous button
 {
     $next = '"submit"';
     $prev = '"hidden"';
 }
-else if($count == $QandAlen - 1) // last question, hide next button
+else if($count == $QandAlen) // last question, hide next button
 {
     $next = '"hidden"';
     $prev = '"submit"';
 } 
-else                             // somewhere in between, show both buttons
+else                         // somewhere in between, show both buttons
     $next = $prev = '"submit"';
 ?>
 
+<input type="hidden" name="count" value=<?php echo $count; ?> />
+<input type="submit" name="sub" value="Submit"/>
 <input type=<?php echo $prev; ?> name="prev" value="Previous question"/>
 <input type=<?php echo $next; ?> name="next" value="Next question"/>
 </form>
 
 <?php 
+//// EVALUATE ANSWER
 if(isset($_POST["sub"]) and isset($_POST["question"])) 
     // user pressed the submit button and an answer is set
 {
-    if($_POST["question"] == $answers[$count])
+    if($_POST["question"] == $answers[$count-1])
         echo "<br/>That's right!";
     else
         echo "<br/>Sorry, that's the wrong answer.";
