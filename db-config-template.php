48cda2ada575e6cf2b678a83894a22e98f7d7b05
David de Kleer <ddekleer@kpnplanet.nl>
2013-12-11 17:03:13 +0100

    Added db-config-template.php, a template for the file db-config.php. This template shows what db-config.php should look like.

---
+++ db-config-template.php
@@ -0,0 +1,10 @@
+<?php 
+/* 
+ * Please change the name of this file to db-config.php if you 
+ * actually want to use it together with with index.php.
+ */
+$dbname = "enter name of database"
+$host = "enter hostname";
+$username = "enter username";
+$password = "enter password";
+?>
