f0b47f674c1eb66b4424defc528d03fb72b4b4ca
David de Kleer <ddekleer@kpnplanet.nl>
2013-12-11 16:55:13 +0100

    Deleted the old index.php and renamed index2.php to index.php. From now on, I will overwrite the past version of index.php with newer versions (and save the old ones with help of keys).

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+//// KEEP TRACK OF THE QUESTION NUMBER
+if(!isset($_COOKIE["count"]) && !isset($_COOKIE["vst"])) 
+{
+    // cookie doesn't exist yet: give $count initial value
+    $count = 1;
+    /* sessvst keeps track of visits to prevent users from loading 
+       the same question again, submitting it and increasing score 
+       (even beyond the amount of questions). */
+    $sessvst = 0;
+}
+else
+{
+    // user returns or submits an answer: don't change $count
+    $count = $_COOKIE["count"];
+    $sessvst = $_COOKIE["vst"] + 1;
+}
+    
+if(isset($_POST["sub"]))
+{
+    // something has been submitted
+    switch($_POST["sub"])
+        // look for a click to the next/previous question
+    {
+    case "Previous question":
+        $sessvst = 0;
+        setcookie("score", 0); // reset score if a user goes back (no cheating)
+        $count = $_COOKIE["count"]-1;
+        break;
+    case "Next question":
+        $sessvst = 0;
+        $count = $_COOKIE["count"]+1;
+        break;
+    }
+}
+setcookie("vst", $sessvst);
+setcookie("count", $count);
+?>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -21,80 +60,98 @@ ini_set("display_errors", 1); /* Debugging: uncomment if needed */
  * type_title=multiple-choice&cat=32&type=1&&id=6272 
  */
 
-//// DEFINE QUESTIONS AND ANSWERS
-$QandA = array(array("What is the name of the first cheese mentioned in".
-         " the <i>Cheese Shop</i> sketch of Monty Python?", "Gouda",
-         "Red Leicester", "Mozzarella", "Edam", "Cheshire"),
-         array("What colour is the pigment chlorophyll?", "Blue",
-         "Red", "Green", "Purple", "Orange"),
-         array("What type of creature is a gecko?", "Bird", "Fish",
-         "Monkey", "Insect", "Lizard"),
-         array("Which US president once claimed to have been". 
-         " 'misunderestimated'?", "George W. Bush", "Jimmy Carter",
-         "Barack Obama", "Gerald Ford", "Ronald Reagan"),
-         array("In which country is the Harz mountain range?", "Austria",
-         "Switzerland", "Spain", "Germany", "Belgium"));
-$answers = array("B", "C", "E", "A", "D");
-$QandAlen = count($QandA);
+// CONNECT TO THE DATABASE
+require_once("db-config.php");
+try
+{ 
+    $db_handle = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
+    /*$db_handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); /* useful for debugging */
+}
+catch (PDOException $e)
+{
+    echo "Connection failed, something's wrong: " . $e->getMessage();
+    exit;
+}
 
-//// KEEP TRACK OF THE QUESTION NUMBER
-if(isset($_POST["count"]))
-    // get the value of the hidden "count" input field (if it has been set)
-    $count = $_POST["count"];
-else
-    // IMPORTANT: $count starts at 1 (because it's question 1), not 0!
-    $count = 1;
-
-if(isset($_POST["next"]) && $count < $QandAlen) 
-    // user pressed next button and there are more questions
-    $count += 1;
-else if(isset($_POST["prev"]) && $count > 1) 
-    // user pressed previous button and there is a previous question
-    $count -= 1;
+$q_handle = $db_handle->query("select count(*) from question");
+$arr = $q_handle->fetch(PDO::FETCH_ASSOC);
+$Qlen = $arr["count(*)"];
  
-//// SHOW QUESTION CORRESPONDING TO COUNT
-echo "<p>" . $QandA[$count-1][0] . "</p>"; 
+//// FETCH A QUESTION FROM THE DATABASE
+$q_handle = $db_handle->query("select q_text from question where q_number = $count");
+$arr = $q_handle->fetch(PDO::FETCH_ASSOC);
+echo "<p>" . $arr["q_text"] . "</p>";
 ?>
 
-<form action=index.php method=post>
+<form action=index2.php method=post>
 
 <?php
-//// SHOW ANSWERS CORRESPONDING TO COUNT
-$answer_values = array('"A"', '"B"', '"C"', '"D"', '"E"');
-for($i = 0; $i < $QandAlen; ++$i) // echo answers to the current question
-    echo '<input type="radio" name="question" value=' . $answer_values[$i] . '/>' . $QandA[$count-1][$i+1] . '<br/>';
+//// FETCH ANSWERS FROM THE DATABASE
+$q_handle = $db_handle->query("select count(*) from choice where q_number = $count");
+$arr = $q_handle->fetch(PDO::FETCH_ASSOC);
+$answer_amount = $arr["count(*)"]; // questions with a variable amount of answers are possible
+$q_handle = $db_handle->query("select c_text from choice where q_number = $count");
+$arr = $q_handle->fetchAll();
+for($i = 1; $i < $answer_amount+1; ++$i) // echo answers to the current question
+{
+    echo '<input type="radio" name="question" value="' . $i . '"/>' . $arr[$i-1]["c_text"] . '<br/>';
+}
 echo '<br/>';
 
 //// SHOW/HIDE THE PREVIOUS/NEXT BUTTONS
+/* Note: the type="hidden" trick as in index.php doesn't work if $count is set by
+   cookies. The problem is that if the script arrives at the last question and
+   you press the "Previous question" button, it advances to the next(!) (non-existing)
+   question. That's why I decided to handle it with display:none (then the right 
+   button will be picked).   
+*/
+$next = $prev = "";
 if($count == 1)              // first question, hide previous button
-{
-    $next = '"submit"';
-    $prev = '"hidden"';
-}
-else if($count == $QandAlen) // last question, hide next button
-{
-    $next = '"hidden"';
-    $prev = '"submit"';
-} 
-else                         // somewhere in between, show both buttons
-    $next = $prev = '"submit"';
+    $prev = 'style = "display:none"';
+else if($count == $Qlen) // last question, hide next button
+    $next = 'style = "display:none"';
 ?>
 
-<input type="hidden" name="count" value=<?php echo $count; ?> />
+<input type="submit" name="sub" value="Previous question" <?php echo $prev; ?>/>
+<input type="submit" name="sub" value="Next question" <?php echo $next; ?>/>
 <input type="submit" name="sub" value="Submit"/>
-<input type=<?php echo $prev; ?> name="prev" value="Previous question"/>
-<input type=<?php echo $next; ?> name="next" value="Next question"/>
+
 </form>
 
 <?php 
 //// EVALUATE ANSWER
-if(isset($_POST["sub"]) and isset($_POST["question"])) 
+if(isset($_POST["sub"]) && $_POST["sub"] == "Submit" && isset($_POST["question"])) 
     // user pressed the submit button and an answer is set
 {
-    if($_POST["question"] == $answers[$count-1])
+    $q_handle = $db_handle->query("select correct from choice where q_number = $count and c_number = " . $_POST["question"]);
+    $arr = $q_handle->fetch(PDO::FETCH_ASSOC);
+    if($arr["correct"])
+    {
+        $score = 1;
         echo "<br/>That's right!";
+    }
     else
+    {
+        $score = 0;
         echo "<br/>Sorry, that's the wrong answer.";
+    }
+    
+    //// KEEP TRACK OF THE SCORE
+    if(isset($_COOKIE["score"])) //add a the value of the score cookie to $score 
+    {
+        if($sessvst < 2) // increase score if the session hasn't been visited multiple times
+            $score += $_COOKIE["score"];
+        else             // otherwise, the score is equal to the already stored score
+            $core = $_COOKIE["score"];
+    }  
+    if ($count == $Qlen) // display score after answering last question
+            echo "<br/><br/>That's it! Your score is " . $score . ".";
+    
+    /* Without sessions or hidden input, it's impossible to create the score cookie 
+       at the start of the script (because answers to questions are determined
+       the end of this script, not already at the beginning!). Setting a cookie 
+       later (like this) doesn't produce any errors on siegfried. */
+    setcookie("score", $score); 
 }
 ?>
 
