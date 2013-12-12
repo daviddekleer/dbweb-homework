59ff10cc0298f1ceb6877652e9dd7562a707978b
David de Kleer <ddekleer@kpnplanet.nl>
2013-12-12 10:52:15 +0100

    Major change to index.php. Now it uses sessions to store question number and score, instead of using cookies. Made the code somewhat cleaner, too.

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php
-//// KEEP TRACK OF THE QUESTION NUMBER
-if(!isset($_COOKIE["count"]) && !isset($_COOKIE["vst"])) 
-{
-    // cookie doesn't exist yet: give $count initial value
-    $count = 1;
-    /* sessvst keeps track of visits to prevent users from loading 
-       the same question again, submitting it and increasing score 
-       (even beyond the amount of questions). */
-    $sessvst = 0;
-}
-else
-{
-    // user returns or submits an answer: don't change $count
-    $count = $_COOKIE["count"];
-    $sessvst = $_COOKIE["vst"] + 1;
-}
-    
-if(isset($_POST["sub"]))
-{
-    // something has been submitted
-    switch($_POST["sub"])
-        // look for a click to the next/previous question
-    {
-    case "Previous question":
-        $sessvst = 0;
-        setcookie("score", 0); // reset score if a user goes back (no cheating)
-        $count = $_COOKIE["count"]-1;
-        break;
-    case "Next question":
-        $sessvst = 0;
-        $count = $_COOKIE["count"]+1;
-        break;
-    }
-}
-setcookie("vst", $sessvst, 0, "", "", 1);
-setcookie("count", $count, 0, "", "", 1);
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -55,12 +20,40 @@ setcookie("count", $count, 0, "", "", 1);
 error_reporting(-1);
 ini_set("display_errors", 1); /* Debugging: uncomment if needed */
 
-/* All questions (except the first) come from http://www.pubquizarea.com/
- * view_question_and_answer_quizzes.php?cat_title=general-knowledge&
- * type_title=multiple-choice&cat=32&type=1&&id=6272 
- */
+//// KEEP TRACK OF THE QUESTION NUMBER AND DO SOME CHEAT PREVENTION
+if(!isset($_SESSION["count"], $_SESSION["submitted"], $_SESSION["score"])) 
+{
+    // session variable do not exist yet: initialize them
+    $_SESSION["count"] = 1;
+    /* $_SESSION["submitted"] is a flag that will be set to 1 if a 
+     * question has been submitted somewhere in the past. This way, 
+     * it prevents cheating (otherwise, it would be possible to keep 
+     * increasing your score, even beyond the amount of questions!) 
+     */
+    $_SESSION["submitted"] = 0;
+    $_SESSION["score"] = 0;
+}
+    
+if(isset($_POST["sub"]))
+{
+    // something has been submitted
+    switch($_POST["sub"]) // look for a click to the next/previous question
+    {
+    case "Previous question":
+        $_SESSION["submitted"] = 0;
+        $_SESSION["score"] = 0; // reset score if a user goes back (no cheating)
+        --$_SESSION["count"];
+        break;
+    case "Next question":
+        $_SESSION["submitted"] = 0;
+        ++$_SESSION["count"];
+        break;
+    }
+}
 
-// CONNECT TO THE DATABASE
+$count = $_SESSION["count"];
+
+//// CONNECT TO THE DATABASE
 require_once("db-config.php");
 try
 { 
@@ -78,6 +71,10 @@ $arr = $q_handle->fetch(PDO::FETCH_ASSOC);
 $Qlen = $arr["count(*)"];
  
 //// FETCH A QUESTION FROM THE DATABASE
+/* All questions (except the first) come from http://www.pubquizarea.com/
+ * view_question_and_answer_quizzes.php?cat_title=general-knowledge&
+ * type_title=multiple-choice&cat=32&type=1&&id=6272 
+ */
 $q_handle = $db_handle->prepare("select q_text from question where q_number = ?");
 $q_handle->bindParam(1, $count);
 $q_handle->execute();
@@ -100,17 +97,17 @@ $q_handle->execute();
 $arr = $q_handle->fetchAll();
 for($i = 1; $i < $answer_amount+1; ++$i) // echo answers to the current question
 {
-    echo '<input type="radio" name="question" value="' . $i . '"/>' . $arr[$i-1]["c_text"] . '<br/>';
+    echo '<input type="radio" name="answer" value="' . $i . '"/>' . $arr[$i-1]["c_text"] . '<br/>';
 }
 echo '<br/>';
 
 //// SHOW/HIDE THE PREVIOUS/NEXT BUTTONS
 /* Note: the type="hidden" trick as in the first version of index.php doesn't work 
-   if $count is set by cookies. The problem is that if the script arrives at the last 
-   question and you press the "Previous question" button, it advances to the next(!) 
-   (non-existing) question. That's why I decided to handle it with display:none (that 
-   way, the right button will be picked).   
-*/
+ * if $count is set by cookies or sessions. The problem is that if the script arrives
+ * at the last question and you press the "Previous question" button, it advances to 
+ * the next(!) (not existing) question. That's why I decided to handle it with 
+ * display:none (that way, the right button will be picked).   
+ */
 $next = $prev = "";
 if($count == 1)              // first question, hide previous button
     $prev = 'style = "display:none"';
@@ -125,42 +122,29 @@ else if($count == $Qlen) // last question, hide next button
 </form>
 
 <?php 
-//// EVALUATE ANSWER
-if(isset($_POST["sub"]) && $_POST["sub"] == "Submit" && isset($_POST["question"])) 
+//// EVALUATE ANSWER AND KEEP TRACK OF SCORE
+if(isset($_POST["sub"], $_POST["answer"]) && $_POST["sub"] == "Submit") 
     // user pressed the submit button and an answer is set
 {
     $q_handle = $db_handle->prepare("select correct from choice where q_number = ? and c_number = ?");
     $q_handle->bindParam(1, $count);
-    $q_handle->bindParam(2, $_POST["question"]);
+    $q_handle->bindParam(2, $_POST["answer"]);
     $q_handle->execute();
     $arr = $q_handle->fetch(PDO::FETCH_ASSOC);
     if($arr["correct"])
-    {
-        $score = 1;
+    {   
         echo "<br/>That's right!";
+        if(!$_SESSION["submitted"]) // increase score if the question hasn't been submitted before
+            {
+            ++$_SESSION["score"];
+            $_SESSION["submitted"] = 1;
+            }
     }
     else
-    {
-        $score = 0;
         echo "<br/>Sorry, that's the wrong answer.";
-    }
-    
-    //// KEEP TRACK OF THE SCORE
-    if(isset($_COOKIE["score"])) //add a the value of the score cookie to $score 
-    {
-        if($sessvst < 2) // increase score if the session hasn't been visited multiple times
-            $score += $_COOKIE["score"];
-        else             // otherwise, the score is equal to the already stored score
-            $core = $_COOKIE["score"];
-    }  
+        
     if ($count == $Qlen) // display score after answering last question
-            echo "<br/><br/>That's it! Your score is " . $score . ".";
-    
-    /* Without sessions or hidden input, it's impossible to create the score cookie 
-       at the start of the script (because answers to questions are determined
-       the end of this script, not already at the beginning!). Setting a cookie 
-       later (like this) doesn't produce any errors on siegfried. */
-    setcookie("score", $score, 0, "", "", 1); 
+            echo "<br/><br/>That's it! Your score is " . $_SESSION["score"] . ".";
 }
 ?>
 
