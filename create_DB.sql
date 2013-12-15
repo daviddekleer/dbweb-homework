d7395403a66566478b6ae9482041d8b7c1c78448
David de Kleer <ddekleer@kpnplanet.nl>
2013-12-15 14:37:51 +0100

    Designed/added a new ER diagram for homework 4, modified create_DB.sql according to this design.

--- create_DB.sql
+++ create_DB.sql
@@ -7,4 +7,24 @@ create table choice(q_number int,
                     c_text varchar(100),
                     correct tinyint, -- made this tinyint instead of bit, because bit doesn't display correctly
                     primary key(q_number, c_number),
-                    foreign key(q_number) references question(q_number));					 
+                    foreign key(q_number) references question(q_number));
+
+create table user(name varchar(20),
+                  password varchar(255), -- store a hash!
+                  primary key(name));
+
+create table first_last_choice(u_name varchar(20),
+                               q_number int,
+                               c_number tinyint,
+                               time_stamp timestamp,
+                               primary key(u_name, q_number, c_number),
+                               foreign key(u_name) references user(name),
+                               foreign key(q_number, c_number) references choice(q_number, c_number));
+
+create table history(u_name varchar(20),
+                     time_taken time,
+                     score int,
+                     primary key(u_name, time_taken, score),
+                     foreign key(u_name) references user(name));
+                     
+                                               
